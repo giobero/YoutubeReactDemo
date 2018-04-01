b4cb925da23b10b98f22341d8446a4b90efd7644
Giorgi Beridze <gio.beri@gmail.com>
2018-04-01 15:24:41 +0400

    Demo React App - Web Client of Youtube API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyBrUCiUIm8lmJJNY4qUZn8BSWelnqAU4-E';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+
+    constructor(params) {
+        super(params)
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+    }
+
+    render() {
+
+        const searchVideo = _.debounce( term => {this.searchVideo(term)}, 300)
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={term => searchVideo(term)} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+
+    searchVideo(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            })
+        });
+    }
+};
+
+ReactDom.render(<App />, document.querySelector('.container'));
